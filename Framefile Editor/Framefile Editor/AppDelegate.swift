fedf10e0c6f1ad24d7f8b356e48ed9ceb6c93674
Michael Fuchs <michi11724dev@googlemail.com>
2017-07-25 18:55:01 +0200

    Project setup

---
+++ Framefile Editor/Framefile Editor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Framefile Editor
+//
+//  Created by Michael Fuchs on 25.07.17.
+//  Copyright © 2017 Michael Fuchs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Framefile Editor/Framefile Editor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Framefile Editor
+//
+//  Created by Michael Fuchs on 25.07.17.
+//  Copyright © 2017 Michael Fuchs. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
